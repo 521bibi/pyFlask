e87ed5f96a516db52f749abe7024e3be5589febe
774717384@qq.com <774717384@qq.com>
2018-10-25 10:48:57 +0800

    渲染

--- firstFlask.py
+++ firstFlask.py
@@ -6,21 +6,67 @@ app.config.from_object(config)
 
 @app.route('/')
 def index():
+    class Person(object):
+        name = 'Lily'
+        age = 18
+
+    p = Person()
+
     context = {
         'username':'bill',
         'gender':'男',
-        'age':21
+        'age':21,
+        'person': p,
+        'website':{
+            'baidu':'www.baidu.com',
+            'google':'www.google.com'
+        }
     }
     return render_template('index.html',**context)
 
 @app.route('/login/')
 def login():
-    return '这是登录页面!'
+    return render_template('login.html')
 
-@app.route('/ask/<is_login>')
-def ask(is_login):
+# @app.route('/questions/<is_login>')
+# def questions(is_login):
+#     if is_login=='1':
+#         user = {
+#             'username':'nike',
+#             'age':18
+#         }
+#         websites = ['baidu','google']
+#         return render_template('questions.html',user=user,websites=websites)
+#     else:
+#         return redirect(url_for('login'))
+
+@app.route('/questions/<is_login>')
+def questions(is_login):
     if is_login=='1':
-        return '这是提问页面！'
+        books = [
+            {
+                'name':'西游记',
+                'author':'吴承恩',
+                'price':120
+            },
+            {
+                'name':'红楼梦',
+                'author':'曹雪芹',
+                'price':130
+            },
+            {
+                'name':'三国演义',
+                'author':'罗贯中',
+                'price':140
+            },
+            {
+                'name':'水浒传',
+                'author':'施耐庵',
+                'price':110
+            },
+
+        ]
+        return render_template('questions.html',books=books)
     else:
         return redirect(url_for('login'))
 
